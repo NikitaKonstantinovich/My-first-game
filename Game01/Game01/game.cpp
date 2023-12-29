d7aaadcb95ff45fc5b6301f267ae8aa2ffa5747a
NikitaKonstantinovich <bib04ka0.7@gmail.com>
2023-12-29 20:32:06 +0300

    Added 3 and 0

--- Game01/Game01/game.cpp
+++ Game01/Game01/game.cpp
@@ -15,4 +15,18 @@ internalVariable void simulateGame(Input* input) {
 	drawRectangle(playerPositionX, playerPositionY, 1, 1, 0x00ff22);
 	drawRectangle(30 + playerPositionX, 30 + playerPositionY, 5, 5, 0xff0022);
 	drawRectangle(-20 + playerPositionX, 20 + playerPositionY, 8, 3, 0x5500ff);
+	drawRectangle(-10 + playerPositionX, -15 + playerPositionY, 8, 8, 0xff00ff);
+	drawRectangle(10 + playerPositionX, -20 + playerPositionY, 8, 7, 0x00ff55);
+
+	//E
+	drawRectangle(-26 + playerPositionX, -30 + playerPositionY, 1, 5, 0xffff22);
+	drawRectangle(-28 + playerPositionX, -30 + playerPositionY, 3, 1, 0xffff22);
+	drawRectangle(-28 + playerPositionX, -26 + playerPositionY, 3, 1, 0xffff22);
+	drawRectangle(-28 + playerPositionX, -34 + playerPositionY, 3, 1, 0xffff22);
+
+	//oO
+	drawRectangle(40 + playerPositionX, 40 + playerPositionY, 1, 5, 0x55ff22);
+	drawRectangle(42 + playerPositionX, 36 + playerPositionY, 3, 1, 0x55ff22);
+	drawRectangle(44 + playerPositionX, 40 + playerPositionY, 1, 5, 0x55ff22);
+	drawRectangle(42 + playerPositionX, 44 + playerPositionY, 3, 1, 0x55ff22);
 }
